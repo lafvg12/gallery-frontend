3317254083318fad5253c6a9be5521b326e04880
Andrés Fernández <lafvg12@hotmail.com>
2022-04-06 04:06:58 -0500

    feat: :art: add animations

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { useState, useEffect } from "react";
 import PhotoAlbum from "react-photo-album";
 import Navbar from "./components/Navbar";
 import "./index.css";
-import { Card } from "../src/components/Card";
+import Lottie from "react-lottie";
+import * as animationData from "./animation.json";
 
 async function getImages() {
   const images = await fetch("https://gallery-app-01.herokuapp.com/image/all");
   const imagesJson = await images.json();
   return imagesJson;
 }
+
+const buttonStyle = {
+  display: "block",
+  margin: "10px auto",
+};
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
 
 function App() {
   const [images, setImages] = useState([]);
@@ -22,7 +37,19 @@ function App() {
   return (
     <div>
       <Navbar />
-      <PhotoAlbum layout="masonry" photos={images} />
+
+      {images.length > 0 ? (
+        <PhotoAlbum layout="masonry" photos={images} />
+      ) : (
+        <Lottie
+          options={defaultOptions}
+          height={400}
+          width={400}
+          isStopped={false}
+          isPaused={false}
+        />
+      )}
+
       {/* {images.map((image) => (
         <div className="card">
           <Card data={image} />
